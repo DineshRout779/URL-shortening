aba322d77fa166ff2deeb90e3747acd0d7f87a77
Dinesh Rout <rddinesh779@gmail.com>
2021-10-13 12:35:48 +0530

    copy link to clipboard

--- js/script.js
+++ js/script.js
@@ -13,32 +13,52 @@ const api = 'https://api.shrtco.de/v2/shorten?url=';
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
-  let fetchUrl = `${api}${inputUrl.value}`;
-  fetch('https://api.shrtco.de/v2/shorten?url=' + inputUrl.value)
-    .then((res) => res.json())
-    .then((data) => {
-      inputUrl.value = '';
-      displayShortenUrls(data.result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  resultDiv.style.display = 'block';
+  if (inputUrl.value !== '') {
+    let fetchUrl = `${api}${inputUrl.value}`;
+    fetch('https://api.shrtco.de/v2/shorten?url=' + inputUrl.value)
+      .then((res) => res.json())
+      .then((data) => {
+        inputUrl.value = '';
+        resultDiv.style.display = 'block';
+        displayShortenUrls(data.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    alert('Please enter a link');
+  }
 });
 
 function displayShortenUrls(urls) {
   resultDiv.innerHTML = `
       <div class="result-url">
-          <p>${urls.full_short_link}</p>
-          <button id="copy-btn" class="form-control submit">Copy</button>
+          <p class="url">${urls.full_short_link}</p>
+          <button class="form-control submit copy">Copy</button>
       </div>
       <div class="result-url">
-          <p>${urls.full_short_link2}</p>
-          <button id="copy-btn" class="form-control submit">Copy</button>
+          <p class="url">${urls.full_short_link2}</p>
+          <button class="form-control submit copy">Copy</button>
       </div>
       <div class="result-url">
-          <p>${urls.full_short_link3}</p>
-          <button id="copy-btn" class="form-control submit">Copy</button>
+          <p class="url">${urls.full_short_link3}</p>
+          <button class="form-control submit copy">Copy</button>
       </div>
     `;
+  const copyBtns = document.querySelectorAll('.copy');
+
+  if (copyBtns.length !== 0) {
+    bindLinkCopyLogic(copyBtns);
+  }
+}
+
+function bindLinkCopyLogic(btns) {
+  const allUrls = document.querySelectorAll('.url');
+  btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      let copyText = allUrls[index].textContent;
+      navigator.clipboard.writeText(copyText);
+      btn.textContent = 'Copied!';
+    });
+  });
 }
